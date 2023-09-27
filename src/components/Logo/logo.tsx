3c77cc6ecade45d98830f7fe9994b9b8aa994a61
Alyona Volkova <alena18252830@gmail.com>
2023-09-28 01:37:42 +0500

    Примерная сборка webpack 5 + react + ts части проекта найденная на просторах инета

---
+++ src/components/Logo/logo.tsx
@@ -0,0 +1,7 @@
+import * as commonStyles from "../../assets/styles/common.scss";
+import * as styles from "./logo.scss";
+import React from "react";
+
+export default function Logo() {
+  return <span className={styles.logo}>Logo</span>;
+}
